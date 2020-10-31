3fb83e2976a8f2d6a5267bd0ae236307a302dee5
Alan Fernandez <alan.f@msn.com>
2020-11-01 00:12:04 +0100

    Initial commit

---
+++ handlers/snippets.js
@@ -0,0 +1,300 @@
+const { db } = require("../util/admin");
+
+// Get all snippets
+exports.getAllSnippets = (req, res) => {
+  db.collection("snippets")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let snippets = [];
+      data.forEach((doc) => {
+        snippets.push({
+          snippetId: doc.id,
+          body: doc.data().body,
+          audio: doc.data().audio,
+          playCount: doc.data().playCount,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+          userImage: doc.data().userImage
+        });
+      });
+      return res.json(snippets);
+    })
+    .catch((err) => console.error(err));
+};
+
+// Post one snippet
+exports.postOneSnippet = (req, res) => {
+  if (req.body.body.trim() === "") {
+    return res.status(400).json({ body: "Body must not be empty" });
+  }
+
+  if (!req.body.audio) {
+    return res.status(400).json({ body: "Must upload audio" });
+  }
+
+  const newSnippet = {
+    body: req.body.body,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl,
+    audio: req.body.audio,
+    playCount: 0,
+    createdAt: new Date().toISOString(),
+    likeCount: 0,
+    commentCount: 0
+  };
+
+  db.collection("snippets")
+    .add(newSnippet)
+    .then((doc) => {
+      const resSnippet = newSnippet;
+      resSnippet.snippetId = doc.id;
+      res.json(resSnippet);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Something went wrong" });
+      console.error(err);
+    });
+};
+
+// Get one snippet
+exports.getSnippet = (req, res) => {
+  let snippetData = {};
+  db.doc(`/snippets/${req.params.snippetId}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Snippet not found" });
+      }
+      snippetData = doc.data();
+      snippetData.snippetId = doc.id;
+      return db
+        .collection("comments")
+        .orderBy('createdAt', 'desc')
+        .where("snippetId", "==", req.params.snippetId)
+        .get();
+    })
+    .then((data) => {
+      snippetData.comments = [];
+      data.forEach((doc) => {
+        snippetData.comments.push(doc.data());
+      });
+      return res.json(snippetData);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
+
+// Comment on a snippet
+exports.commentOnSnippet = (req, res) => {
+  if(req.body.body.trim() === '') return res.status(400).json({ comment: 'Must not be empty'});
+
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    snippetId: req.params.snippetId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl
+  }
+
+  db.doc(`/snippets/${req.params.snippetId}`).get()
+    .then(doc =>{
+      if(!doc.exists){
+        return res.status(404).json({ error: 'Snippet not found' });
+      }
+      return doc.ref.update({ commentCount: doc.data().commentCount + 1});
+    })
+    .then(() => {
+      return db.collection('comments').add(newComment); 
+    })
+    .then(() => {
+      res.json(newComment);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+};
+
+// Like a snippet
+exports.likeSnippet = (req, res) => {
+  const likeDocument = db.collection('likes')
+  .where('userHandle', '==', req.user.handle)
+  .where('snippetId', '==', req.params.snippetId).limit(1);
+  
+  const snippetDocument = db.doc(`/snippets/${req.params.snippetId}`);
+
+  let snippetData;
+
+  snippetDocument.get()
+    .then(doc => {
+      if(doc.exists){
+        snippetData = doc.data();
+        snippetData.snippetId = doc.id;
+        return likeDocument.get(); 
+      } else {
+        return res.status(404).json({ error: 'Snippet not found'});
+      }
+    })
+    .then(data => {
+      if(data.empty){
+        return db.collection('likes').add({
+          snippetId: req.params.snippetId,
+          userHandle: req.user.handle,
+          createdAt: new Date().toISOString()
+        })
+        .then(() => {
+          snippetData.likeCount++
+          return snippetDocument.update({ likeCount: snippetData.likeCount });
+        })
+        .then(() => {
+          return res.json(snippetData);
+        })
+      } else {
+        return res.status(400).json({ error: 'Snippet already liked'});
+      }
+    })
+    .catch(err => {
+      console.log(error);
+      res.status(500).json({ err: error.code })
+    });
+};
+
+// Unlike a snippet
+exports.unlikeSnippet = (req, res) => {
+  const likeDocument = db.collection('likes')
+  .where('userHandle', '==', req.user.handle)
+  .where('snippetId', '==', req.params.snippetId).limit(1);
+  
+  const snippetDocument = db.doc(`/snippets/${req.params.snippetId}`);
+
+  let snippetData;
+
+  snippetDocument.get()
+    .then(doc => {
+      if(doc.exists){
+        snippetData = doc.data();
+        snippetData.snippetId = doc.id;
+        return likeDocument.get(); 
+      } else {
+        return res.status(404).json({ error: 'Snippet not found'});
+      }
+    })
+    .then(data => {
+      if(data.empty){
+        return res.status(400).json({ error: 'Snippet not liked'});
+      } else {
+        return db.doc(`/likes/${data.docs[0].id}`).delete()
+          .then(() => {
+            snippetData.likeCount--;
+            return snippetDocument.update({ likeCount: snippetData.likeCount });
+          })
+          .then(() => {
+            res.json(snippetData);
+          })
+      }
+    })
+    .catch(err => {
+      console.log(error);
+      res.status(500).json({ err: error.code })
+    });
+};
+
+// Play a snippet (Logged user)
+exports.playSnippetLogged = (req, res) => {
+  const playDocument = db.collection('plays')
+  .where('userHandle', '==', req.user.handle)
+  .where('snippetId', '==', req.params.snippetId).limit(1);
+  
+  const snippetDocument = db.doc(`/snippets/${req.params.snippetId}`);
+
+  let snippetData;
+
+  snippetDocument.get()
+    .then(doc => {
+      if(doc.exists){
+        snippetData = doc.data();
+        snippetData.snippetId = doc.id;
+        return playDocument.get(); 
+      } else {
+        return res.status(404).json({ error: 'Snippet not found'});
+      }
+    })
+    .then(data => {
+      if(data.empty){
+        return db.collection('plays').add({
+          snippetId: req.params.snippetId,
+          userHandle: req.user.handle,
+          createdAt: new Date().toISOString()
+        })
+        .then(() => {
+          snippetData.playCount++
+          return snippetDocument.update({ playCount: snippetData.playCount });
+        })
+        .then(() => {
+          return res.json(snippetData);
+        })
+      } else {
+        return res.status(400).json({ error: 'Snippet already played'});
+      }
+    })
+    .catch(err => {
+      console.log(error);
+      res.status(500).json({ err: error.code })
+    });
+};
+
+// Play a snippet (Not logged user)
+exports.playSnippetNotLogged = (req, res) => {
+  const snippetDocument = db.doc(`/snippets/${req.params.snippetId}`);
+
+  let snippetData;
+
+  snippetDocument.get()
+    .then(doc => {
+      if(doc.exists){
+        snippetData = doc.data();
+        snippetData.snippetId = doc.id;
+        snippetData.playCount++;
+        snippetDocument.update({ playCount: snippetData.playCount });
+        return res.json(snippetData);
+      } else {
+        return res.status(404).json({ error: 'Snippet not found'});
+      }
+    })
+    .catch(err => {
+      console.log(error);
+      res.status(500).json({ err: error.code })
+    });
+};
+
+// Delete a snippet
+exports.deleteSnippet = (req, res) => {
+  const document = db.doc(`/snippets/${req.params.snippetId}`);
+  document.get()
+    .then(doc => {
+      if(!doc.exists){
+        return res.status(404).json({ error: 'Snippet not found'});
+      } else if(doc.data().userHandle !== req.user.handle) {
+        return res.status(403).json({ error: 'Unathorized'});
+      } else {
+        return document.delete();
+      }
+    })
+    .then(() => {
+      res.json({ message: 'Snippet deleted succesfully'});
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+// Play one Snippet
+exports.playCount = (req, res) => {
+  
+}
---
+++ index.js
@@ -0,0 +1,151 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const FBAuth = require('./util/fbAuth');
+const { db } = require('./util/admin');
+const cors = require('cors');
+
+const {
+  getAllSnippets,
+  postOneSnippet,
+  getSnippet,
+  commentOnSnippet,
+  likeSnippet,
+  unlikeSnippet,
+  deleteSnippet,
+  playSnippetLogged,
+  playSnippetNotLogged,
+} = require('./handlers/snippets');
+
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead
+} = require('./handlers/users');
+
+const app = express();
+app.use(cors());
+
+// Snippet routes
+app.get('/snippets', getAllSnippets);
+app.post('/snippet', FBAuth, postOneSnippet);
+app.get('/snippet/:snippetId', getSnippet);
+app.delete('/snippet/:snippetId', FBAuth, deleteSnippet);
+app.get('/snippet/:snippetId/play', FBAuth, playSnippetLogged);
+app.get('/snippet/:snippetId/playNotLogged', playSnippetNotLogged);
+app.get('/snippet/:snippetId/like', FBAuth, likeSnippet);
+app.get('/snippet/:snippetId/unlike', FBAuth, unlikeSnippet);
+app.post('/snippet/:snippetId/comment', FBAuth, commentOnSnippet);
+
+// Users routes
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
+
+// Create notification on like
+exports.createNotificationOnLike = functions.region('europe-west1').firestore.document('likes/{id}')
+.onCreate((snapshot) => {
+  return db.doc(`/snippets/${snapshot.data().snippetId}`).get()
+    .then(doc => {
+      if(doc.exists && doc.data().userHandle !== snapshot.data().userHandle){
+        return db.doc(`/notifications/${snapshot.id}`).set({
+          createdAt: new Date().toISOString(),
+          recipient: doc.data().userHandle,
+          sender: snapshot.data().userHandle,
+          type: 'like',
+          read: false,
+          snippetId: doc.id
+        });
+      }
+    })
+    .catch(err => 
+      console.error(err));
+});
+
+// Delete notification on unlike
+exports.deleteNotificationOnUnlike =  functions.region('europe-west1').firestore.document('likes/{id}')
+.onDelete((snapshot) => {
+  return db.doc(`/notifications/${snapshot.id}`)
+    .delete()
+    .catch(err => {
+      console.error(err);
+      return;
+    });
+});
+
+// Create notification on comment
+exports.createNotificationOnComment = functions.region('europe-west1').firestore.document('comments/{id}')
+.onCreate((snapshot) => {
+  return db.doc(`/snippets/${snapshot.data().snippetId}`).get()
+    .then(doc => {
+      if(doc.exists && doc.data().userHandle !== snapshot.data().userHandle){
+        return db.doc(`/notifications/${snapshot.id}`).set({
+          createdAt: new Date().toISOString(),
+          recipient: doc.data().userHandle,
+          sender: snapshot.data().userHandle,
+          type: 'comment',
+          read: false,
+          snippetId: doc.id
+        });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      return;
+    });
+});
+
+// Update user image on change
+exports.onUserImageChange = functions.region('europe-west1').firestore.document('/users/{userId}')
+.onUpdate((change) => {
+  console.log(change.before.data());
+  console.log(change.after.data());
+  if(change.before.data().imageUrl !== change.after.data().imageUrl){
+    console.log('Image has changed');
+    const batch = db.batch();
+    return db.collection('snippets').where('userHandle', '==', change.before.data().handle).get()
+      .then((data) => {
+        data.forEach(doc => {
+          const snippet = db.doc(`/snippets/${doc.id}`);
+          batch.update(snippet, { userImage: change.after.data().imageUrl});
+        })
+        return batch.commit();
+      });
+  } else return true;
+});
+
+// Clean on snippet delete
+exports.onSnippetDelete = functions.region('europe-west1').firestore.document('snippets/{snippetId}')
+.onDelete((snapshot, context) => {
+  const snippetId = context.params.snippetId;
+  const batch = db.batch();
+  return db.collection('comments').where('snippetId', '==', snippetId).get()
+    .then(data => {
+      data.forEach(doc => {
+        batch.delete(db.doc(`/comments/${doc.id}`));
+      })
+      return db.collection('likes').where('snippetId', '==', snippetId).get()
+    })
+    .then(data => {
+      data.forEach(doc => {
+        batch.delete(db.doc(`/likes/${doc.id}`));
+      })
+      return db.collection('notifications').where('snippetId', '==', snippetId).get()
+    })
+    .then(data => {
+      data.forEach(doc => {
+        batch.delete(db.doc(`/notifications/${doc.id}`));
+      })
+      return batch.commit();
+    })
+    .catch(err => console.error(err));
+})
